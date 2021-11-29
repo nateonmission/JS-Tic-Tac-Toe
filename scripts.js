3443315fa7bf4c7e781c0b6095a0fc441c03c869
Nate Allen <nateonmission@gmail.com>
2021-11-29 14:35:11 -0500

    refactored checkForWinner

--- scripts.js
+++ scripts.js
@@ -1,8 +1,12 @@
 let playOn = true;
-let gameHistory = []
+let gameHistory = [];
 let gameCounter = 0;
 let turnCounter = 1;
+let availableMoves = [true, true, true, true, true, true, true, true, true];
 
+
+const gb = document.querySelector("#game-board");
+const sq0 = document.querySelector("#sq0");
 const sq1 = document.querySelector("#sq1");
 const sq2 = document.querySelector("#sq2");
 const sq3 = document.querySelector("#sq3");
@@ -11,8 +15,8 @@ const sq5 = document.querySelector("#sq5");
 const sq6 = document.querySelector("#sq6");
 const sq7 = document.querySelector("#sq7");
 const sq8 = document.querySelector("#sq8");
-const sq9 = document.querySelector("#sq9");
 const statusArea = document.querySelector("#status")
+let squares = [sq0, sq1, sq2, sq3, sq4, sq5, sq6, sq7, sq8];
 
 
 class Player {
@@ -38,7 +42,8 @@ const clearBoard = () => {
     sq6.innerText = '';
     sq7.innerText = '';
     sq8.innerText = '';
-    sq9.innerText = '';
+    sq0.innerText = '';
+    availableMoves = [true, true, true, true, true, true, true, true, true];
 } 
 
 
@@ -101,100 +106,75 @@ let player = player1;
 
 const checkForWinner = (player) => {
     let sigel = player.sigel;
-    
+    console.log(availableMoves)
     switch (true) {
         case (
+            sq0.innerText === sq1.innerText &&
             sq1.innerText === sq2.innerText &&
-            sq2.innerText === sq3.innerText &&
-            sq3.innerText === sigel
+            sq2.innerText === sigel
         ):
-            console.log(`${sigel} WINS!`);
-            takeTurns();
-            gameEnd(player);
-            break;
+            return 1;
         case (
+            sq3.innerText === sq4.innerText &&
             sq4.innerText === sq5.innerText &&
-            sq5.innerText === sq6.innerText &&
-            sq4.innerText === sigel
+            sq5.innerText === sigel
         ):
-            console.log(`${sigel} WINS!!!!`)
-            takeTurns();
-            gameEnd(player);
-            break;
+            return 1;
         case (
+            sq6.innerText === sq7.innerText &&
             sq7.innerText === sq8.innerText &&
-            sq8.innerText === sq9.innerText &&
-            sq9.innerText === sigel
+            sq8.innerText === sigel
         ):
-            console.log(`${sigel} WINS!!!!`)
-            takeTurns();
-            gameEnd(player);
-            break;
+            return 1;
+        case (
+            sq0.innerText === sq3.innerText &&
+            sq3.innerText === sq6.innerText &&
+            sq6.innerText === sigel
+        ):
+            return 1;
         case (
             sq1.innerText === sq4.innerText &&
             sq4.innerText === sq7.innerText &&
             sq7.innerText === sigel
         ):
-            console.log(`${sigel} WINS!!!!`)
-            takeTurns();
-            gameEnd(player);
-            break;
+            return 1;
         case (
             sq2.innerText === sq5.innerText &&
             sq5.innerText === sq8.innerText &&
             sq8.innerText === sigel
         ):
-            console.log(`${sigel} WINS!!!!`)
-            takeTurns();
-            gameEnd(player);
-            break;
+            return 1;
         case (
-            sq3.innerText === sq6.innerText &&
-            sq6.innerText === sq9.innerText &&
-            sq9.innerText === sigel
+            sq2.innerText === sq4.innerText &&
+            sq4.innerText === sq6.innerText &&
+            sq6.innerText === sigel
         ):
-            console.log(`${sigel} WINS!!!!`)
-            takeTurns();
-            gameEnd(player);
-            break;
+            return 1;
         case (
-            sq3.innerText === sq5.innerText &&
-            sq5.innerText === sq7.innerText &&
-            sq7.innerText === sigel
+            sq0.innerText === sq4.innerText &&
+            sq4.innerText === sq8.innerText &&
+            sq8.innerText === sigel
         ):
-            console.log(`${sigel} WINS!!!!`)
-            takeTurns();
-            gameEnd(player);
-            break;
-        case (
-            sq1.innerText === sq5.innerText &&
-            sq5.innerText === sq9.innerText &&
-            sq9.innerText === sigel
-        ):
-            console.log(`${sigel} WINS!!!!`)
-            takeTurns();
-            gameEnd(player);
-            break;
-        case (sq1.innerText !== '' && sq2.innerText !== '' && sq3.innerText !== '' && 
-            sq4.innerText !== '' && sq5.innerText !== '' && sq6.innerText !== '' && 
-            sq7.innerText !== '' && sq8.innerText !== '' && sq9.innerText !== ''):
-            console.log("DRAW");
-            takeTurns();
-            gameEnd('Draw');
-            break;
+            return 1;
+        case (sq0.innerHTM !== '' && sq1.innerText !== '' && sq2.innerText !== '' && 
+            sq3.innerText !== '' && sq4.innerText !== '' && sq5.innerText !== '' && 
+            sq6.innerText !== '' && sq7.innerText !== '' && sq8.innerText !== ''):
+            return 0;
+            
         default:
-            takeTurns();
-            break;
+            return null;
+            
     }
 }
 
-const gameClick = (sqr) => {
+const gameClick = (sqr, spot) => {
     if(sqr.innerText === ''){
         let sigel = player.getSigel()
         sqr.setAttribute("class",`${player.color}`);
         sqr.innerHTML = `${sigel}`;
+        availableMoves[spot] = false;
     } else {
-        throw "already clciked"
+        throw "already clicked"
     }
 }
 
@@ -209,70 +189,110 @@ const takeTurns = () => {
         p1Div.setAttribute("class", "activeBlue");
         p2Div.setAttribute("class", "player-div");
     }
-    
-
 }
+
+
+
+
+const aiPlayer = (squares, availableMoves, player) => {
+    let otherPlayer = player === player1 ? player2 : player1;
+
+    for (let i = 0; i < squares.length; i++){
+        if (availableMoves[i]) {
+
+            squares[i].click();
+
+        }
+    }
+}
+
+
+
+
+sq0.addEventListener("click", () => {
+    if (playOn) {
+        gameClick(sq0, 0)
+    }
+})
 
 sq1.addEventListener("click", () => {
     if (playOn) {
-        gameClick(sq1)
-        checkForWinner(player, turnCounter);
+        gameClick(sq1, 1)
     }
-
 })
 
 sq2.addEventListener("click", () => {
     if (playOn) {
-        gameClick(sq2)
-        checkForWinner(player, turnCounter);
+        gameClick(sq2, 2)
     }
 })
 
 sq3.addEventListener("click", () => {
     if (playOn) {
-        gameClick(sq3)
-        checkForWinner(player, turnCounter);
+        gameClick(sq3, 3)
     }
 })
 
 sq4.addEventListener("click", () => {
     if (playOn) {
-        gameClick(sq4)
-        checkForWinner(player, turnCounter);
+        gameClick(sq4, 4)
     }
 })
 
 sq5.addEventListener("click", () => {
     if (playOn) {
-        gameClick(sq5)
-        checkForWinner(player, turnCounter);
+        gameClick(sq5, 5)
     }
 })
 
 sq6.addEventListener("click", () => {
     if (playOn) {
-        gameClick(sq6)
-        checkForWinner(player, turnCounter);
+        gameClick(sq6, 6)
     }
 })
 
 sq7.addEventListener("click", () => {
     if (playOn) {
-        gameClick(sq7)
-        checkForWinner(player, turnCounter);
+        gameClick(sq7, 7)
     }
 })
 
 sq8.addEventListener("click", () => {
     if (playOn) {
-        gameClick(sq8)
-        checkForWinner(player, turnCounter);
+        gameClick(sq8, 8)
     }
 })
 
-sq9.addEventListener("click", () => {
-    if (playOn) {
-        gameClick(sq9)
-        checkForWinner(player, turnCounter);
+gb.addEventListener("click", () => {
+    if(playOn){
+        console.log('gb')
+        let gameEnded = checkForWinner(player, turnCounter);
+
+        if (gameEnded === null){
+            takeTurns();
+        } else if (gameEnded === 0) {
+                console.log("DRAW");
+                gameEnd('Draw');
+                takeTurns();
+        } else if (gameEnded === 1) {
+            console.log(`${player.sigel} WINS!!!!`)
+            gameEnd(player);
+            takeTurns();
+        }
+
+        if (player.name === "Computer") {
+            aiPlayer(squares, availableMoves, player);
+        }
+
+        // let i = 0;
+        // squares.forEach( sq => {
+        //     if (sq.innerText === '') {
+        //         console.log(`${i}`);
+                
+        //     }
+        //     i++
+        // })
     }
-})
+}, false)
+
+
